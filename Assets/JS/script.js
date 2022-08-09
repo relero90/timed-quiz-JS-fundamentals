8334dd4c2d72bfb96a236ec99ee3a516c2a8ff21
relero90 <relero90@gmail.com>
2022-08-09 16:03:28 -0600

    added global variables for start screen and question containers; added css class .hidden; coded display switch from start screen to questions[0]

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,32 +1,39 @@
 // Variable Declarations
 var start = document.querySelector("#start");
+var startScrn = document.querySelector("#start-container");
+var questions = document.querySelectorAll("#question-container");
+// Arrays of correct/incorrect buttons
 var correct = document.querySelectorAll("#correct");
 var incorrect = document.querySelectorAll("#incorrect");
+// User feedback string
 var feedback = document.querySelectorAll("#user-feedback");
+
 var userScore = 0;
+
 // Functions
 
 // Event Listeners
 // < ---------------------------------------- >
 // When user clicks on the start button
 start.addEventListener("click", function () {
-  console.log("hello");
   // start timer function
-  // disappear start screen
-  // display question 1
+
+  // disappear start screen & show question 1
+  startScrn.setAttribute("class", "hidden");
+  questions[0].setAttribute("class", "visible");
 });
 
 // When user clicks on a correct answer
 // For each button in the correct array - run scoreUp function
 
-correct.forEach((correct) => {
+correct.forEach((correct, i) => {
   correct.addEventListener("click", function handleClick(event) {
     // add points to value of userScore
-    userScore = userScore + 17;
+    userScore = userScore + 10;
+    localStorage.setItem("score", userScore);
     // alert user
-    // feedback = feedback.textContent(
-    //   "Correct! Your current score is " + userScore
-    // );
+    feedback[i].textContent =
+      "Correct! Your current score is " + userScore + ".";
     console.log(userScore);
     correct.setAttribute(
       "style",
@@ -37,10 +44,11 @@ correct.forEach((correct) => {
 });
 
 // When user clicks on an incorrect answer
-incorrect.forEach((incorrect) => {
+incorrect.forEach((incorrect, i) => {
   incorrect.addEventListener("click", function handleClick(event) {
     // alert user
     feedback.forEach((feedback) => {
+      // JS returning error feedback.textContent
       feedback = feedback.textContent(
         "Correct! Your current score is " + userScore
       );
